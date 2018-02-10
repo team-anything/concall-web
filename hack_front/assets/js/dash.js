0ff29142c3b47300e83ed63db6151c9b7eb28bd5
Neel Shah <neelshah.1998@gmail.com>
2018-02-10 22:58:41 +0530

    init front end

---
+++ hack_front/assets/js/dash.js
@@ -0,0 +1,23 @@
+// $(function() {
+
+//   $(document).on("click", function(e) {
+//     var $item = $(".rad-dropmenu-item");
+//     if ($item.hasClass("active")) {
+//       $item.removeClass("active");
+//     }
+//   });
+
+//   $(".rad-toggle-btn").on('click', function() {
+//     $(".rad-sidebar").toggleClass("rad-nav-min");
+//     $(".rad-body-wrapper").toggleClass("rad-nav-min");
+//     setTimeout(function() {
+//       initializeCharts();
+//     }, 200);
+//   });
+
+//   $(".rad-dropdown >.rad-menu-item").on('click', function(e) {
+//     e.stopPropagation();
+//     $(".rad-dropmenu-item").removeClass("active");
+//     $(this).next(".rad-dropmenu-item").toggleClass("active");
+//   });
+// });
