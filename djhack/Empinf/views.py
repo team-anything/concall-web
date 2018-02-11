eb6b86c2aa5a0202e068848941a32433cde71fa0
Neel Shah <neelshah.1998@gmail.com>
2018-02-11 10:30:45 +0530

    /

--- djhack/Empinf/views.py
+++ djhack/Empinf/views.py
@@ -8,18 +8,26 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     Employees = Employee.objects.all();
     employ=request.user.username
-    print(employ)
+    print("XXXXXXXXXXXXXXXXX",employ)
     emps = Employee.objects.get(name=employ)
     iss=Iss.objects.all()
     string='/Empinf/'+emps.slug
+    print("XXXXXXXXXXXXXXXXXXXX", string)
     return redirect(string)
 
 def emp_detail(request, slug):
     #return HttpResponse(slug)
     emps = Employee.objects.get(slug=slug)
-    iss=Iss.objects.all()
+    iss=Iss.objects.all().order_by("-priority")
     return render(request, 'Empinf/issues.html', { 'emps': emps,'Iss':iss })
 #
 # @login_required(login_url="/accounts/login/")
 # def article_create(request):
 #     return render(request, 'articles/article_create.html')
+
+def record(request):
+    employee=request.user.username
+    emp=Employee.objects.get(name=employee)
+    pending=emp.issues_pending
+    resolved=emp.total_issues
+    return render(request, 'Empinf/record.html', {'pen':pending, 'res':resolved})
---
+++ djhack/static/chart1.js
@@ -0,0 +1,82 @@
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Issues status'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: data1
+    }]
+});
+
+Highcharts.chart('container1', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Issues Tagwise'
+    },
+    xAxis: {
+        categories: ['Tech','Electronics','Stationary','Fitness']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total Issues'
+        },
+        stackLabels: {
+            enabled: true,
+            style: {
+                fontWeight: 'bold',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+            }
+        }
+    },
+    legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+    },
+    tooltip: {
+        headerFormat: '<b>{point.x}</b><br/>',
+        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: true,
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+            }
+        }
+    },
+    series:data2
+});
